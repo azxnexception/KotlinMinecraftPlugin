816119000397f4e98fa24833fb851579352596c5
azxn! <asnasmurf01@gmail.com>
2024-10-02 14:21:58 +0200

    added Teleport Command, IDK IF WORKING

--- src/main/kotlin/de/azxn/kotlinMinecraftPlugin/commands/TeleportCommand.kt
+++ src/main/kotlin/de/azxn/kotlinMinecraftPlugin/commands/TeleportCommand.kt
@@ -1,13 +1,29 @@
 package de.azxn.kotlinMinecraftPlugin.commands
 
 import de.azxn.kotlinMinecraftPlugin.utils.FormattingUtils
+import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 class TeleportCommand : CommandExecutor {
-    override fun onCommand(p0: CommandSender, p1: Command, p2: String, p3: Array<out String>?): Boolean {
-        p0.sendMessage(FormattingUtils.PREFIX.append(FormattingUtils.empty("<red>Soon...")))
+    override fun onCommand(sender: CommandSender, command: Command, strings: String, string: Array<out String>?): Boolean {
+        if (sender is Player ) {
+            var player = sender
+            if (strings.length == 1) {
+                var target = Bukkit.getPlayerExact(strings[0].toString())
+                if (target != null) {
+                    player.teleport(target.getLocation())
+                } else {
+                    player.sendMessage(FormattingUtils.PREFIX.append(FormattingUtils.empty("<red>Player not found!")))
+                }
+            } else {
+                player.sendMessage(FormattingUtils.PREFIX.append(FormattingUtils.empty("<red>Usage: /teleport <player>")))
+            }
+        }
+
         return true
+
     }
 }
